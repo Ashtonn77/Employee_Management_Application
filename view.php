38761801a31da2f872e5d3db99f43a087369e883
ashton naidoo <ashtonrbay@gmail.com>
2020-04-19 16:41:44 +0200

    added view.php

---
+++ view.php
@@ -0,0 +1,34 @@
+<?php
+require_once('include/DB.php');
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Insert into Database</title>
+</head>
+<link rel="stylesheet" href="include/style.css">
+<body>
+
+<div class="">
+<table width='1000' border='5' align='center'>
+<caption>View from Database</caption>
+
+<tr>
+<th>ID</th>
+<th>Name</th>
+<th>SSN</th>
+<th>Department</th>
+<th>Salary</th>
+<th>Home Address</th>
+</tr>
+
+</table>
+</div>
+    
+</body>
+</html>
